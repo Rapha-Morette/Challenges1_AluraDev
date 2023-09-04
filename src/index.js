096adc261d3c81a132d43bd08670f80511d0352b
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-09-04 17:04:13 -0300

    Adicionando funcionalidades de java script para os Hovers e começando a mudar a cor do editor de código

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 const linguagem = document.querySelector("#linguagem_programacao");
 const areaDoCodigo = document.querySelector(".editor_codigo");
-
+const backgroundCor = document.querySelector(".editor__codigo");
 const descricao = document.querySelector("#descricao_projeto");
 const botao = document.querySelector(".botao_highlight");
+const btnCorBackground = document.querySelector(".btn_background");
 
 function aplicaHighlight() {
   const codigo = areaDoCodigo.innerText;
   areaDoCodigo.innerHTML = `<code class="preview hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de código"</code>`;
-  areaDoCodigo.querySelector('code').textContent = codigo;
-  hljs.highlightElement(areaDoCodigo.querySelector('code'))
+  areaDoCodigo.querySelector("code").textContent = codigo;
+  hljs.highlightElement(areaDoCodigo.querySelector("code"));
 }
 
-botao.addEventListener('click', ()=> {
-    aplicaHighlight();
-})
+botao.addEventListener("click", () => {
+  aplicaHighlight();
+});
+
+function mudaCorEditor(cor) {
+  switch (cor) {
+    case 'azul':
+      backgroundCor.style.backgroundColor = "#6bd1ff"
+      break;
+    case 'verde':
+      backgroundCor.style.backgroundColor = "#27C93F"
+      break;
+  }
+  // if(cor === 'azul'){
+  //   console.log("azul")
+  //   backgroundCor.style.backgroundColor = "#fff"
+  // } else if(btnCorBackground === 'verde') {
+  //   backgroundCor.style.backgroundColor = "#000"
+  // }
+}
