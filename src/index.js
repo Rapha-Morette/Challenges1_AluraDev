5ebb07ccbe983177e36525932a3280d40b91e5a3
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-09-05 14:55:22 -0300

    botões de cores finalizados

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const areaDoCodigo = document.querySelector(".editor_codigo");
 const backgroundCor = document.querySelector(".editor__codigo");
 const descricao = document.querySelector("#descricao_projeto");
 const botao = document.querySelector(".botao_highlight");
-const btnCorBackground = document.querySelector(".btn_background");
+const btnCorBackground = document.querySelectorAll(".btn_background");
 
 function aplicaHighlight() {
   const codigo = areaDoCodigo.innerText;
@@ -19,16 +19,33 @@ botao.addEventListener("click", () => {
 function mudaCorEditor(cor) {
   switch (cor) {
     case 'azul':
+      removeBotaoSelecionado();
       backgroundCor.style.backgroundColor = "#6bd1ff"
+      selecionaBotao();
       break;
     case 'verde':
+      removeBotaoSelecionado();
       backgroundCor.style.backgroundColor = "#27C93F"
+      selecionaBotao();
+      break;
+    case 'rosa':
+      removeBotaoSelecionado();
+      backgroundCor.style.backgroundColor = "#FF6BCD"
+      selecionaBotao();
       break;
   }
-  // if(cor === 'azul'){
-  //   console.log("azul")
-  //   backgroundCor.style.backgroundColor = "#fff"
-  // } else if(btnCorBackground === 'verde') {
-  //   backgroundCor.style.backgroundColor = "#000"
-  // }
+}
+
+function removeBotaoSelecionado() {
+  btnCorBackground.forEach(btn => {
+    btn.classList.remove('botao_selecionado')
+  });
+}
+
+function selecionaBotao(){
+  btnCorBackground.forEach(btn => {
+    btn.addEventListener('click', function(){
+      this.classList.add('botao_selecionado');
+    })
+  })
 }
